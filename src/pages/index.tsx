39b7ae561885257e21c37bbdf23ca633b2911de5
Luis Gabriel <janco7249@gmail.com>
2021-11-16 16:04:22 -0400

    refactor: remove import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { Home } from '../components/Home/Home'
-import { useDarkMode } from '../hooks/useDarkMode'
+// import { useDarkMode } from '../hooks/useDarkMode'
 
 const HomePage: NextPage = () => {
   return (
