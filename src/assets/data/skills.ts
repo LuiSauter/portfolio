ad5b640be7b95a48fc00bf70e4b6d2304d0a866a
Luis Gabriel <janco7249@gmail.com>
2021-11-15 22:17:24 -0400

    create icons and array skills

---
+++ src/assets/data/skills.ts
@@ -0,0 +1,14 @@
+export default [
+  'React',
+  'NextJS',
+  'Redux',
+  'TypeScript',
+  'JavaScript',
+  'HTML5',
+  'CSS3',
+  'SASS',
+  'NodeJS',
+  'MongoDB',
+  'ExpressJS',
+  'MongooseJS',
+]
