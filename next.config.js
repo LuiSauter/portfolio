0f40f117c0a46d0b0ff95e1a7bd3bd4341efdbf3
Luis Gabriel <janco7249@gmail.com>
2021-11-15 22:12:58 -0400

    inital commit 🔧

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  domains: [
+    'localhost'
+  ]
 }
