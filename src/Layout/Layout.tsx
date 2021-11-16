9549416933b2f21f20adef8dfa52769a4704643e
Luis Gabriel <janco7249@gmail.com>
2021-11-15 22:19:08 -0400

    feat: add layout default

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Nav } from '../components/Nav/Nav'
+import { LayoutStyle } from './LayoutStyle'
+import Head from 'next/head'
+
+interface Props {
+  children: JSX.Element | JSX.Element[]
+}
+
+export const Layout = ({children}: Props) => {
+  return (
+    <>
+      <Head>
+        <link rel='icon' href='/sauterdev.jpg' />
+        <meta
+          name='description'
+          content='Portfolio frontend developer Reactjs, Nextjs, typescript, Javascript '
+        />
+      </Head>
+      <LayoutStyle>
+        <Nav />
+        {children}
+        {/* footer */}
+      </LayoutStyle>
+    </>
+  )
+}
---
+++ src/Layout/LayoutStyle.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LayoutStyle = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+  width: 90%;
+`
