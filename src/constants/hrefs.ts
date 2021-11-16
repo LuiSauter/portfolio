541d318a134cb310760a78fdcfab188300d4d228
Luis Gabriel <janco7249@gmail.com>
2021-11-15 22:21:46 -0400

    fix: custom constants for hrefs

---
+++ src/constants/hrefs.ts
@@ -0,0 +1,10 @@
+export const HOME_HREF = '/'
+
+export const PROJECTS_HREF = '/projects'
+
+export const EXPERIENCE_HREF = '/experience'
+
+export const CONTACT_HREF = '/contact'
+
+export const BLOG_HREF = '/blog'
+
