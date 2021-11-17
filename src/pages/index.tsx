82072ef3ff7b73c27e9ef44e60c2d90249f00a2e
Luis Gabriel <janco7249@gmail.com>
2021-11-16 22:38:18 -0400

    remove comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Home } from '../components/Home/Home'
-// import { useDarkMode } from '../hooks/useDarkMode'
 
 const HomePage: NextPage = () => {
   return (
