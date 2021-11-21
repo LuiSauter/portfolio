a766251702379aa6cc2c4951434592c11757e74f
Luis Gabriel <janco7249@gmail.com>
2021-11-20 22:00:43 -0400

    feat: add new icons

--- src/assets/data/skills.ts
+++ src/assets/data/skills.ts
@@ -1,15 +1,14 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default [
-  'React',
-  'NextJS',
-  'Redux',
-  'TypeScript',
-  'JavaScript',
-  'HTML5',
-  'CSS3',
-  'SASS',
-  'NodeJS',
-  'MongoDB',
-  'ExpressJS',
-  'MongooseJS',
+export const skillsData = [
+  // {skill: 'React', color: '#61dafb'},
+  // {skill: 'NextJS', color: '#000000'},
+  // {skill: 'Redux', color: '#764abc'},
+  // {skill: 'TypeScript', color: '#2376c5'},
+  // {skill: 'JavaScript', color: '#efd81d'},
+  // {skill: 'HTML5', color: '#dd4b25'},
+  // {skill: 'CSS3', color: '#1f6db9'},
+  // {skill: 'SASS', color: '#d25191'},
+  // {skill: 'NodeJS', color: '#78c83a'},
+  // {skill: 'MongoDB', color: '#378d4d'},
+  // {skill: 'ExpressJS', color: '#000000'},
+  // {skill: 'MongooseJS', color: '#841400'},
 ]
