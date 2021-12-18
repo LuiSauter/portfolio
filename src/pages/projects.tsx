2ed9d49e5c425962c8546807ca961ec373d40eb0
Luis Gabriel <janco7249@gmail.com>
2021-12-18 16:20:15 -0400

    refactor: update function styled

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import type { NextPage } from 'next'
 import InfiniteScroll from 'react-infinite-scroll-component'
+import Image from 'next/image'
 
 import projects from '../assets/data/projects'
 import { ProjectItem } from '../components/Project/ProjectItem'
@@ -25,11 +26,21 @@ const Projects: NextPage = (): JSX.Element => {
         />
         <link rel='canonical' href='https://sauterdev.vercel.app/projects' />
       </Head>
-      <Titleh2>Portafolio</Titleh2>
-      <p>
-        Proyectos educativos, personales y profesionales con los que me sigo
-        formando dia a dia.
-      </p>
+      <TitleProjects>
+        <Image
+          src='/images/portfolio-projects.svg'
+          width={100}
+          height={100}
+          title='Luis G. Janco'
+          priority={true}
+          aria-label={'xd'}
+          alt={'xd'}
+        />
+        <h2>
+          Proyectos educativos, personales y profesionales con los que me sigo
+          formando dia a dia.
+        </h2>
+      </TitleProjects>
       <ProjectContainer>
         <InfiniteScroll
           dataLength={dataDisplayed.length}
@@ -61,6 +72,42 @@ const Projects: NextPage = (): JSX.Element => {
 
 export default Projects
 
-const Titleh2 = styled.h2`
-  font-size: 1.8rem;
+const TitleProjects = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 90vh;
+  padding: 0 1vw 2rem;
+  position: relative;
+  width: 100%;
+  margin: auto;
+  h2 {
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: 900;
+    z-index: 2;
+  }
+  span {
+    img {
+      min-width: 300px;
+      max-width: 500px;
+      filter: drop-shadow(10px 5px 0.2rem #27272773);
+      /* box-shadow: 0 20px 25px -5px #000000aa; */
+    }
+  }
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    span {
+      width: 100%;
+      img {
+        width: 100%;
+        min-width: 400px;
+      }
+    }
+    h2 {
+      width: 50%;
+      font-size: 2rem;
+    }
+  }
 `
