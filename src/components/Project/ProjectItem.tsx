411f696e334cddc7532c2b1b53c5b410c418308c
Luis Gabriel <janco7249@gmail.com>
2021-12-18 16:21:14 -0400

    style: add blur img and flex dinamic

--- src/components/Project/ProjectItem.tsx
+++ src/components/Project/ProjectItem.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
-import { CardItemProject } from './style'
+import { CardDescription, CardItemProject, Footer } from './style'
 import Image from 'next/image'
 import Link from 'next/link'
+
+const shimmer = (w: any, h: any) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+const toBase64 = (str: any) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
 
 export const ProjectItem = ({
   authors,
@@ -15,18 +34,7 @@ export const ProjectItem = ({
     <Link href={url}>
       <a target='_blank'>
         <CardItemProject className='card-project'>
-          <div className='card-project__img'>
-            <Image
-              alt={title}
-              src={image}
-              layout='fill'
-              objectFit='cover'
-              objectPosition='top center'
-              aria-label={title}
-              priority={true}
-            />
-          </div>
-          <div>
+          <CardDescription className='card-project__description'>
             <h3>{title}</h3>
             <p>{description}</p>
             <article>
@@ -42,7 +50,7 @@ export const ProjectItem = ({
                 </p>
               ))}
             </article>
-            <footer>
+            <Footer>
               <ul>
                 {tags.map((tag, index) => (
                   <li className='border-color-skills' key={index}>
@@ -50,7 +58,25 @@ export const ProjectItem = ({
                   </li>
                 ))}
               </ul>
-            </footer>
+            </Footer>
+          </CardDescription>
+          <div className='card-project__img'>
+            <Image
+              alt={title}
+              src={image}
+              layout='intrinsic'
+              width={500}
+              height={260}
+              placeholder='blur'
+              blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                shimmer(700, 475)
+              )}`}
+              // loading='lazy'
+              objectFit='cover'
+              objectPosition='top center'
+              aria-label={title}
+              priority={true}
+            />
           </div>
         </CardItemProject>
       </a>
--- src/components/Project/style.ts
+++ src/components/Project/style.ts
@@ -4,110 +4,138 @@ import { colorDark } from "../../styles/theme";
 export const ProjectContainer = styled.section`
   margin: 2rem 0;
   min-height: 100vh;
-  width: 100%;
+  margin: 0 -1.1rem;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   .infinite-scroll-component {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+    display: flex;
+    flex-direction: column;
     width: 100%;
-    gap: 2rem;
+    gap: 5rem;
     a {
       color: inherit;
       text-decoration: none;
       transition: transform 500ms;
-      &:hover {
-        transform: scale(1.01);
-      }
     }
   }
-  @media screen and (max-width: 768px) {
-    .infinite-scroll-component {
-      grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    }
+  @media screen and (min-width: 768px) {
+    margin-top: 1rem;
   }
 `
 export const CardItemProject = styled.article`
-  padding: 1rem;
-  border-radius: 1rem;
-  height: 100%;
-  position: relative;
   display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  gap: 1rem;
+  flex-direction: column;
+  gap: 0.5em;
+  width: 100%;
+  border-top: 1px solid #a1a1a138;
+  border-bottom: 1px solid #a1a1a138;
+  padding: 1rem 0;
+  position: relative;
+  height: 100%;
   .card-project__img {
     position: relative;
-    border-radius: 0.9rem;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    min-height: 200px;
     border: 1px solid transparent;
+    width: 100%;
+    display: flex;
+    justify-content: center;
     span {
-      overflow-y: hidden;
-      span {
-        background-color: red;
-      }
+      max-width: 480px;
+      height: 100%;
+      width: 100%;
+      min-height: 260px;
+      max-height: 480px;
       img {
-        margin: auto;
-        border-radius: 0.9rem;
         width: 100%;
         height: 100%;
-        min-height: 300px;
-        display: none;
-      }
-      transition: transform 1000ms;
-      &:hover {
-        transform: scale(1.3);
       }
     }
   }
-  div {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    border-top: 1px solid transparent;
+    border-bottom: 1px solid transparent;
     width: 100%;
-    gap: 0.4rem;
-    article p {
-      position: relative;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      img {
-        border-radius: 50%;
-      }
+    /* height: 70vh; */
+    .card-project__img {
+      width: 100%;
       span {
-        display: block;
-        margin-left: 0.4rem;
-        font-size: 0.9rem;
+        width: 100%;
+        border-radius: 1rem;
+        /* box-shadow: 0 20px 25px -5px rgb(0 0 0/0.1),
+          0 8px 10px -6px rgb(0 0 0/0.1); */
+        box-shadow: 0 0px 10px 3px rgb(0 0 0 / 11%),
+          0 0px 20px -50px rgb(0 0 0 / 11%);
       }
     }
   }
-  ul {
-    position: relative;
+`
+
+export const CardDescription = styled.div`
+  position: relative;
+  padding: 0 1rem;
+  h3 {
+    font-size: 1.5rem;
+  }
+  p {
+    font-size: 0.95rem;
+    padding: 0.7rem 0;
+  }
+  article {
     display: flex;
     flex-direction: row;
-    flex-wrap: wrap;
-    gap: 0.3rem;
-    li {
-      list-style: none;
-      background-color: transparent;
-      border-radius: 0.6rem;
-      padding: 0rem 0.7rem 0.15rem;
+    align-items: center;
+    width: 100%;
+    p {
+      width: 100%;
       display: flex;
-      span {
-        text-align: center;
-        font-size: 0.9rem;
-        padding: 0;
-        margin: 0;
+      align-items: center;
+      justify-content: center;
+      gap: 0.5rem;
+      span img {
+        border-radius: 50%;
       }
     }
   }
-  @media screen and (max-width: 768px) {
-    gap: 0.4rem;
-    flex-direction: column;
+  @media screen and (min-width: 768px) {
+    display: grid;
+    place-content: center;
+    place-items: center;
+    margin: 0 0 0 2rem;
+    width: 60%;
+    border-radius: 1rem;
+    padding: 0.5rem 1rem;
+    box-shadow: 0 5px 8px 5px rgb(0 0 0 / 8%);
+    p {
+      padding: 0.7rem 1.5rem;
+    }
+  }
+`
+
+export const Footer = styled.footer`
+  ul {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    overflow-x: auto;
+    gap: 0.8rem;
+    opacity: 80%;
+    padding: 0 0.5rem 0.5rem;
+    li {
+      font-size: 0.8rem;
+      font-weight: bold;
+      list-style: none;
+      padding: 0.1rem 0.5rem;
+      border-radius: 0.8rem;
+    }
+  }
+  @media screen and (min-width: 768px) {
+    ul {
+      padding: 1rem 0;
+      flex-wrap: wrap;
+      li {
+        padding: 0.1rem 0.5rem;
+      }
+    }
   }
 `
