2ef6647c260ba1a72ff5388f67c2b2f7bddd2a9e
Luis Gabriel <janco7249@gmail.com>
2022-03-31 16:22:39 -0400

    [style] changes in project of design

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Head from 'next/head'
 import type { NextPage } from 'next'
 import Image from 'next/image'
+import React from 'react'
+import Head from 'next/head'
+import styled from 'styled-components'
+
+import { colorDark } from '../styles/theme'
+import * as icons from '../assets/icons'
 
 import projects from '../assets/data/projects'
 import { ProjectItem } from '../components/Project/ProjectItem'
 import { ProjectContainer } from '../components/Project/style'
-import styled from 'styled-components'
 import { DownArrow } from '../components/Home/DownArrow'
 import { downArrow } from '../assets/icons'
+import { useDarkMode } from '../hooks/useDarkMode'
 
 const Projects: NextPage = (): JSX.Element => {
   const projectsReverse = [...projects].reverse()
+  const { isDarkMode } = useDarkMode()
   return (
     <>
       <Head>
@@ -23,19 +28,13 @@ const Projects: NextPage = (): JSX.Element => {
         <link rel='canonical' href='https://sauterdev.vercel.app/projects' />
       </Head>
       <TitleProjects>
-        <Image
-          src='/images/portfolio-projects.svg'
-          width={100}
-          height={100}
-          title='Luis G. Janco'
-          priority={true}
-          aria-label={'xd'}
-          alt={'xd'}
-        />
-        <h2>
-          Proyectos educativos, personales y profesionales con los que sigo
-          formándome día a día.
-        </h2>
+        <div className='hero'>{icons.projectHeroDev}</div>
+        <div className='title'>
+          <h2 className='title-h2'>
+            Proyectos educativos, personales y profesionales{' '}
+            <span>con los que sigo formándome día a día.</span>
+          </h2>
+        </div>
       </TitleProjects>
       <div style={{ position: 'relative', transform: 'translateX(-2%)' }}>
         <DownArrow href='#projects'>{downArrow}</DownArrow>
@@ -53,14 +52,30 @@ const Projects: NextPage = (): JSX.Element => {
           />
         ))}
         {projectsReverse.length % 2 !== 0 && <br />}
-        <img
-          className='current-streak'
-          src='https://github-readme-streak-stats.herokuapp.com/?user=LuiSauter&theme=blue-green'
-        />
-        <img
-          className='current-streak'
-          src='https://github-readme-stats.vercel.app/api?username=LuiSauter&theme=blue-green'
-        />
+        <div className='stats'>
+          <img
+            className='current-streak'
+            src={`https://github-readme-streak-stats.herokuapp.com/?user=LuiSauter&theme=${
+              isDarkMode.isDark ? 'react' : 'default'
+            }`}
+          />
+        </div>
+        <div className='stats'>
+          <img
+            className='current-streak'
+            src={`https://github-readme-stats.vercel.app/api?username=LuiSauter&theme=${
+              isDarkMode.isDark ? 'react' : 'default'
+            }`}
+          />
+        </div>
+        <div className='stats'>
+          <img
+            className='current-streak'
+            src={`https://github-readme-stats.vercel.app/api/top-langs/?username=LuiSauter&layout=compact&theme=${
+              isDarkMode.isDark ? 'react' : 'default'
+            }`}
+          />
+        </div>
       </ProjectContainer>
     </>
   )
@@ -69,40 +84,43 @@ const Projects: NextPage = (): JSX.Element => {
 export default Projects
 
 const TitleProjects = styled.div`
-  scroll-snap-align: start;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  flex-shrink: 0;
-  height: 100%;
-  padding: 2rem;
-  width: 100%;
-  h2 {
-    text-align: center;
-    font-size: 1.5rem;
-    font-weight: 900;
-    z-index: 2;
+  position: relative;
+  .hero {
+    position: relative;
+    svg {
+      width: 100%;
+      height: 100%;
+      opacity: 0.9;
+    }
   }
-  span {
-    img {
-      min-width: 300px;
-      max-width: 500px;
+  .title {
+    .title-h2 {
+      font-size: 1.8rem;
+      line-height: 1.9rem;
+      text-align: center;
+      text-shadow: 1px 2px 3px #19191969;
+      span {
+        color: ${colorDark.colorTextThird};
+        font-weight: 900;
+      }
     }
   }
   @media screen and (min-width: 768px) {
-    flex-direction: row;
-    height: 90vh;
-    span {
-      width: 100%;
-      img {
-        width: 100%;
-        min-width: 400px;
-      }
+    position: relative;
+    .hero {
+      position: relative;
+      height: 90vh;
     }
-    h2 {
-      width: 50%;
-      font-size: 2rem;
+    .title {
+      position: absolute;
+      top: 50%;
+      .title-h2 {
+        font-size: 4rem;
+        line-height: 3.4rem;
+        text-align: center;
+      }
     }
   }
 `
