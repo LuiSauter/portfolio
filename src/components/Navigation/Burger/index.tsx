d4cf4f1641830717911048089e7e6a027d5a1098
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-19 23:55:08 -0400

    feat: adding burger button for menu

---
+++ src/components/Navigation/Burger/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './burger.module.css'
+
+const Burger: React.FC = () => {
+  return (
+    <div className={style.burger}>
+      <span />
+      <span />
+      <span />
+    </div>
+  )
+}
+
+export default Burger
