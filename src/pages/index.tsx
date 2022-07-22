bd8e2463bf112b19a4ec10aa8c67b05c00c608a1
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-21 22:56:37 -0400

    feat: adding more content in home hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Fragment } from 'react'
-import { SocialNetwork } from '../components/SocialNetwork/SocialNetwork'
-// import { SkillsContainer } from '../components/Skills/SkillsContainer'
-// import { SocialNetwork } from '../components/SocialNetwork/SocialNetwork'
-// import { Sumary } from '../components/Sumary/Sumary'
+import Link from 'next/link'
+import { Fragment, useEffect, useState } from 'react'
+import { SkillsContainer } from '../components/Skills/SkillsContainer'
+import { Sumary } from '../components/Sumary/Sumary'
 import style from '../styles/pages/index.module.css'
 
+function getRandom(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+let cleanup = true
 const HomePage: NextPage = () => {
+  const [mounted, setMounted] = useState(false)
+  const bgGradient = getRandom(1, 4)
+
+  useEffect(() => {
+    cleanup && setMounted(true)
+    return () => {
+      cleanup = false
+    }
+  }, [])
+
+  const paps = mounted && `gradient-${bgGradient}` || ''
+
   return (
     <Fragment>
       <Head>
@@ -16,7 +31,7 @@ const HomePage: NextPage = () => {
       </Head>
       <section className={style.section}>
         <div className={style.hero}>
-          <div className={style.shadow} />
+          <div aria-label={`gradient-${bgGradient}`} className={style.shadow} />
           <figure>
             <Image
               src='/sauterdev.jpg'
@@ -28,27 +43,41 @@ const HomePage: NextPage = () => {
           </figure>
         </div>
         <div className={style.heroContent}>
-          <h1 className={style.meTitle}>
-            <span className={style.clip}>Luis Gabriel Janco</span>
+          <h1 aria-label={paps} className={style.meTitle}>
+            <span title='JavaScript Lover' className={style.clip}>
+              Sauterdev
+            </span>
           </h1>
           <p>
-            Full-Stack Web Developer and <span>JS</span> Lover
+            Full-Stack Web Developer and <span className={style.js}></span>
+            <span className={style.lover}>Lover</span>
           </p>
           <h2 className={style.tagline}>
-            Apasionado de la tecnología, amante del Open-Source y del trabajo en
-            equipo.
+            Passionate about technology, lover of Open-Source and teamwork.
           </h2>
+          <div className={style.actions}>
+            <Link href='/about'>
+              <a aria-label={paps} className={style.action}>
+                About me
+              </a>
+            </Link>
+            <Link href='/contact'>
+              <a className={style.actionExtra}>Contact</a>
+            </Link>
+            <Link href='/contact'>
+              <a className={style.actionExtra}>Stack</a>
+            </Link>
+          </div>
         </div>
-        <SocialNetwork />
+        {/* <SocialNetwork /> */}
       </section>
+      {/* <Features></Features> */}
       {/* <div className='skills-sumary'>
         <h3 id='skills'>
-          <a href='#skills'>{clip}</a>
           Habilidades
         </h3>
         <SkillsContainer />
         <h3 id='resume'>
-          <a href='#resume'>{clip}</a>
           Resumen
         </h3>
         <Sumary />
