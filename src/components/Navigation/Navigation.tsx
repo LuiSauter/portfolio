3a5ca2428e23b84a766929bfcde0a700e61045dc
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-24 00:18:04 -0400

    fix: remove variable unused

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
 import style from './navigation.module.css'
 import Burger from './Burger'
