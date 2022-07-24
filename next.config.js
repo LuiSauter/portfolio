ad75658bed77afedcfe47bd4fcf4d10add4e1823
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-24 00:36:28 -0400

    chore: add domains permission

--- next.config.js
+++ next.config.js
@@ -3,10 +3,17 @@ module.exports = {
   reactStrictMode: true,
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: [
+      'https://gpvc.arturio.dev',
+      'gpvc.arturio.dev',
+      'https://sauterdev.vercel.app'
+    ],
   },
   domains: [
     'localhost',
     'https://sauterdev.vercel.app',
-    '192.168.0.10:3000'
+    '192.168.0.10:3000',
+    'https://gpvc.arturio.dev',
+    'gpvc.arturio.dev'
   ]
 }
