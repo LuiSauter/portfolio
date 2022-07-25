5c98917f131704146e455dca56faec52e8b3fd3d
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-24 21:49:32 -0400

    feat: add the project item with sound on hover mouse

--- src/components/Project/ProjectItem.tsx
+++ src/components/Project/ProjectItem.tsx
@@ -1,52 +1,68 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
-import * as icon from '../../assets/icons'
-
-const shimmer = (w: any, h: any) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
-
-const toBase64 = (str: any) =>
-  typeof window === 'undefined'
-    ? Buffer.from(str).toString('base64')
-    : window.btoa(str)
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import useSound from '../../hooks/useSound'
+import useThemes from '../../hooks/useThemes'
+import style from './projectItem.module.css'
 
 export const ProjectItem = ({ description, tags, title, url }: Project) => {
-  const github = 'https://github.com/LuiSauter/'
+  const [mounted, setMounted] = useState(false)
+  const { playSound } = useSound()
+  let cleanup = true
+  useEffect(() => {
+    cleanup && setMounted(true)
+    return () => {
+      cleanup = false
+    }
+  }, [])
+
+  const mouseEnter = () => {
+    playSound('/sounds/hover.mp3')
+  }
+  const {checkIfIsDark} = useThemes()
+
+  const handleClick= () => {
+    playSound('/sounds/button-2.mp3')
+  }
+
   return (
-    <article className='card-item'>
-      <div className='card-item__header'>
-        <span className='book'>{icon.book}</span>
-        <h2>{title}</h2>
-        <span className='visibility'>Public</span>
-        <img
-          src={`https://badgen.net/github/commits/LuiSauter/${url}`}
-          title={url}
-          alt='Luis Gabriel Janco'
-        />
-        <img src='https://badgen.net/badge/Open%20Source%20%3F/Yes%21/blue?icon=github' alt='Luis Gabriel Janco' />
-      </div>
-      <p className='card-item__description'>{description}</p>
-      <ul className='card-item__tags'>
-        {tags.map((tag, index: number) => (
-          <li key={index}>
-            <span className='dot' style={{ color: tag.color }}>
-              •
-            </span>
-            <span className='skill'>{tag.skill}</span>
-          </li>
-        ))}
-      </ul>
-    </article>
+    <Link href={`https://github.com/LuiSauter/${url}`}>
+      <a target='_blank' className={style.figure} onMouseEnter={mouseEnter} onClick={handleClick}>
+        {mounted && checkIfIsDark() ? (
+          <img
+            src={`https://github-readme-stats.vercel.app/api/pin/?username=LuiSauter&repo=${url}&title_color=FFFFFFDE&icon_color=EBEBEB99&text_color=EBEBEB99&bg_color=1f2028&border_color=1f2028&hide_rank=false`}
+            alt=''
+          />
+        ) : (
+          <img
+            src={`https://github-readme-stats.vercel.app/api/pin/?username=LuiSauter&repo=${url}&title_color=213547&icon_color=3C3C3CB3&text_color=3C3C3CB3&bg_color=ffffff&border_color=3c3c3c1f&hide_rank=false`}
+            alt=''
+          />
+        )}
+      </a>
+    </Link>
+    // <article className={style.article}>
+    //   <div className={style.header}>
+    //     <div>
+    //       <span className={style.bookIcon}>{icon.book}</span>
+    //       <h2>{title}</h2>
+    //       <span className={style.visibility}>Public</span>
+    //     </div>
+    //     <img
+    //       src={`https://badgen.net/github/commits/LuiSauter/${url}`}
+    //       title={url}
+    //       alt='Luis Gabriel Janco'
+    //     />
+    //   </div>
+    //   <p className={style.description}>{description}</p>
+    //   <ul className={style.languages}>
+    //     {tags.map((tag, index: number) => (
+    //       <li key={index} className={style.langItem}>
+    //         <span className={style.circle} style={{ backgroundColor: tag.color }} />
+    //         <span>{tag.skill}</span>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </article>
   )
 }
