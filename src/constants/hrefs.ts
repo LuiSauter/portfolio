3f6282f10a9ef753c67498646757532a16c569b1
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-24 21:52:10 -0400

    fix: remove unnecessary links

--- src/constants/hrefs.ts
+++ src/constants/hrefs.ts
@@ -1,6 +1,6 @@
 export const hrefs = [
   { name: 'Projects', href: '/projects', disabled: false },
-  { name: 'Experience', href: '/experience', disabled: false },
-  { name: 'Contact', href: '/contact', disabled: false },
+  { name: 'Experience', href: '/', disabled: false },
+  { name: 'Contact', href: '/', disabled: false },
   { name: 'Blog', href: '/', disabled: true },
 ]
