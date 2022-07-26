b71131c76245b874223541b524f77b071c8acb01
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-25 20:47:08 -0400

    refactor: adding more controls the audio

--- src/hooks/useSound.tsx
+++ src/hooks/useSound.tsx
@@ -1,11 +1,18 @@
+type PropsSound = {
+  audioSrc: string,
+  volume: number,
+  time?: number
+}
 
-const useSound = (): { playSound: (audioSrc: string) => void } => {
-  async function playSound(audioSrc: string = ''): Promise<void> {
-    try {
-      await new Audio(audioSrc).play()
-    } catch (error) {
-      // any error handling
-    }
+const useSound = (): { playSound: ({ audioSrc, volume, time}: PropsSound) => void } => {
+  function playSound({ audioSrc= '', volume = 1, time = 0 }: PropsSound): void {
+    const audio = new Audio(audioSrc)
+    audio.volume = volume
+    audio.currentTime = time
+    audio.play().catch(() => {/* not sounds */})
+    setTimeout(() => {
+      audio.pause()
+    }, 300)
   }
   return { playSound }
 }
