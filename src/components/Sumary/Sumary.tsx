6c172747950e052f85fa68299fd98c364a3245a6
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-25 20:45:39 -0400

    refactor: create function to get counts of days

--- src/components/Sumary/Sumary.tsx
+++ src/components/Sumary/Sumary.tsx
@@ -7,18 +7,27 @@ import style from './sumary.module.css'
 const frontStack = Object.values(technologiesIcons.frontend).length
 const backStack = Object.values(technologiesIcons.backend).length
 
+type PropsDate = {
+  init: Date
+  current: Date
+}
+
+function countDays({ init, current }: PropsDate): number {
+  const difference: number = init.getTime() - current.getTime()
+  const totalDays = Math.ceil(difference / (1000 * 3600 * 24))
+  return totalDays
+}
+
 const Sumary: React.FC = () => {
   const [coffeeCount, setCoffeeCount] = useState('010')
   useEffect(() => {
     const initialDate = new Date("08/01/2021")
     const currentDay = new Date(Date.now())
-    const DifferenceInTime = currentDay.getTime() - initialDate.getTime();
-    const DifferenceInDays = DifferenceInTime / (1000 * 3600 * 24)
     setCoffeeCount(
-      new Intl.NumberFormat('en').format(Math.floor(DifferenceInDays))
+      new Intl.NumberFormat('en').format(
+        Math.abs(countDays({ init: initialDate, current: currentDay }))
+      )
     )
-    return () => {
-    }
   }, [])
 
   return (
