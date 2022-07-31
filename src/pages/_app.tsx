74e1c9f90e5d95f8a017cf9ab0468b8fdc3ae8be
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-31 10:10:10 -0400

    feat: set google analytics in portfolio

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,27 @@
+/* eslint-disable @next/next/inline-script-id */
+import { Fragment } from 'react'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import { Layout } from '../components/Layout/Layout'
 import { ThemeProvider } from '../context/ThemeProvider'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }: AppProps | any) {
   return (
-    <ThemeProvider forcedTheme={Component.theme || undefined}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <Fragment>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy='lazyOnload'>
+        {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {page_path: window.location.pathname,});`}
+      </Script>
+      <ThemeProvider forcedTheme={Component.theme || undefined}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </Fragment>
   )
 }
 
