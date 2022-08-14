8830de9f7fb7ca8de032144734638a0280193c46
Luis Gabriel Janco <janco7249@gmail.com>
2022-08-14 13:52:21 -0400

    feat: add analytics and metrics with google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,19 +10,40 @@ class MyDocument extends Document {
           <meta charSet='utf-8' />
           <meta
             name='description'
-            content='Proffesional Portfolio for Web Developer, Front-End Developer, Back-End Developer, Full-Stack Developer, and more.'
+            content='Front-end developer specializing in the javascript language giving value to software products.'
           />
-          <meta property='og:type' content='website' />
           <meta property='og:title' content='Luis Gabriel Janco' />
+          <meta property='og:type' content='website' />
+          <meta property='og:url' content='https://sauterdev.vercel.app' />
+          <meta
+            property='og:image'
+            content='https://sauterdev.vercel.app/sauterdev.jpg'
+          />
           <meta
             property='og:description'
-            content='Proffesional Portfolio for Web Developer, Front-End Developer, Back-End Developer, Full-Stack Developer, and more.'
+            content='Front-end developer specializing in the javascript language giving value to software products.'
+          />
+          <meta
+            property='og:sauterdev'
+            content='Luis Gabriel Janco, Front-End Developer'
+          />
+          <meta
+            name='twitter:image:alt'
+            content='Luis Gabriel Janco - Front-End Developer'
           />
           <meta name='twitter:card' content='summary_large_image' />
           <link rel='icon' href='/sauterdev.jpg' />
           <meta
             name='google-site-verification'
             content='nk6ETUfejHtjHnkXEBzgWhOOX_k-m3iSSVxpoEbVzGM'
+          />
+          <script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {page_path: window.location.pathname,});`,
+            }}
           />
         </Head>
         <body>
