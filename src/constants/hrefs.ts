34f496d0c0dfef2546267dc3364d20c9096bb67b
Luis Gabriel Janco <janco7249@gmail.com>
2022-08-20 12:15:30 -0400

    refactor: change href of experience

--- src/constants/hrefs.ts
+++ src/constants/hrefs.ts
@@ -1,6 +1,6 @@
 export const hrefs = [
   { name: 'Projects', href: '/projects', disabled: false },
-  { name: 'Experience', href: '/', disabled: false },
-  { name: 'Contact', href: '/', disabled: false },
+  { name: 'Experience', href: '/me#experience', disabled: false },
+  { name: 'Contact', href: '/', disabled: true },
   { name: 'Blog', href: '/', disabled: true },
 ]
